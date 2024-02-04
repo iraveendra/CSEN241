959d7be9d2ee025863f6d6f947f3f000e6b615d5
iraveendra <149436689+iraveendra@users.noreply.github.com>
2024-02-03 21:35:28 -0800

    Renamed a log file

--- HW1/Docker/tests.sh
+++ HW1/Docker/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for script in "cpu1000.sh" "cpu2000.sh" "fileiorrw.sh" "fileioseq.sh" "mem1M.sh" "mem512K.sh"; do
+for script in "cpu1000.sh" "cpu2000.sh" "fileiorrw.sh" "fileioseq.sh" "mem1G.sh" "mem512K.sh"; do
 	for i in 1 2 3 4 5; do
 		echo "Running iteration $i of $script"
 		./$script >> /logs/log_${script%.*}_$i.txt
